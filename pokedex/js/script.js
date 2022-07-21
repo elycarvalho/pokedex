f2c6e653f3a71392a32485ba0e914998ae09991c
Ely Carvalho <elycarvalho3@gmail.com>
2022-07-21 10:21:40 -0300

    Add files via upload

--- pokedex/js/script.js
+++ pokedex/js/script.js
@@ -1,33 +1,53 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 905
 const colors = {
 	fire: '#fddfdf',
 	grass: '#defde0',
 	eletric: '#fcf7de',
 	water: '#def3fd',
-	ground: '#f4e7da'
-	rock: '#d5d5d4'
-	fairy: '#fceaff'
-	poison: '#98d7a5'
-	bug: '#f8d5a3'
-	dragon: '#97b3e6'
-	psychic: '#eaeda1'
-    flying: 'f5f5f5'
-    fighting: 'e6e0d4'
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+    flying: 'f5f5f5',
+    fighting: 'e6e0d4',
     normal: '#f5f5f5'
 }
 
 const fetchPokemons = async () => {
-	for(let i =1; i <= pokemon_count; i++) {
-		await getPkemon(i)
+	for(let i = 1; i <= pokemon_count; i++) {
+		await getPokemon(i)
 	}
 }
 
-const getPkemon = async (id) => {
+const getPokemon = async (id) => {
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 	const res = await fetch(url)
 	const data = await res.json()
-	console.log(data)
+	createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+	pokemonEl.classList.add('pokemon')
+
+	const pokemonInnerHTML = `
+	<div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+    	<span class="number">${pokemon.id}</span>
+    	<h3 class="name">${pokemon.name}</h3>
+    	<small class="tipo">Tipo: <span></span></small>
+    </div>
+	`
+    
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+	poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemons()
